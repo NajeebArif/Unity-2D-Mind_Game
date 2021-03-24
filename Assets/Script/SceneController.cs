76601df7f828c0710504af10a6154134690a41f0
Najeeb Arif <NajeebArif786@outlook.com>
2021-03-24 14:23:28 +0530

    Adding grid view with 8 cards on table

--- Assets/Script/SceneController.cs
+++ Assets/Script/SceneController.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
+
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
+
     [SerializeField] private MemoryCard originalCard;
     [SerializeField] private Sprite[] sprites;
     // Start is called before the first frame update
     void Start()
     {
-        int id = Random.Range(0, sprites.Length);
-        originalCard.setCard(id, sprites[id]);
+        Vector3 startingPosition = originalCard.transform.position;
+
+        for(int i =0; i < gridCols; i++) {
+            for(int j = 0; j < gridRows; j++) {
+                MemoryCard card;
+                if (i == 0 && j == 0)
+                    card = originalCard;
+                else
+                    card = Instantiate(originalCard) as MemoryCard;
+                int id = Random.Range(0, sprites.Length);
+                originalCard.setCard(id, sprites[id]);
+                float posX = (offsetX * i) + startingPosition.x;
+                float posY = -(offsetY * j) + startingPosition.y;
+                card.transform.position = new Vector3(posX, posY, startingPosition.z);
+            }
+        }
+
     }
 
     // Update is called once per frame
