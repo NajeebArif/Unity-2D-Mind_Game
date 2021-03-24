8c34607f48ba97ddb3d8fcc46c32271dda4a90c0
Najeeb Arif <NajeebArif786@outlook.com>
2021-03-24 14:32:30 +0530

    Making the cards to be layed out in pairs

--- Assets/Script/SceneController.cs
+++ Assets/Script/SceneController.cs
@@ -18,15 +18,22 @@ public class SceneController : MonoBehaviour
     {
         Vector3 startingPosition = originalCard.transform.position;
 
+        int[] numbers = { 0,0,1,1,2,2,3,3,4,4};
+        numbers = suffleArray(numbers);
+
         for(int i =0; i < gridCols; i++) {
             for(int j = 0; j < gridRows; j++) {
                 MemoryCard card;
+
                 if (i == 0 && j == 0)
                     card = originalCard;
                 else
                     card = Instantiate(originalCard) as MemoryCard;
-                int id = Random.Range(0, sprites.Length);
+
+                int index = j * gridCols + i;
+                int id = numbers[index];
                 originalCard.setCard(id, sprites[id]);
+
                 float posX = (offsetX * i) + startingPosition.x;
                 float posY = -(offsetY * j) + startingPosition.y;
                 card.transform.position = new Vector3(posX, posY, startingPosition.z);
@@ -39,5 +46,16 @@ public class SceneController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private int[] suffleArray(int[] numbers) {
+        int[] newArray = numbers.Clone() as int[];
+        for(int i = 0; i < newArray.Length; i++) {
+            int temp = newArray[i];
+            int r = Random.Range(0, newArray.Length);
+            newArray[i] = newArray[r];
+            newArray[r] = temp;
+        }
+        return newArray;
     }
 }
